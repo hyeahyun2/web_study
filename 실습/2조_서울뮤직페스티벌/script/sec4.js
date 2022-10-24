c38b0fa25ab29787b80f88f89f3541ac582f2450
hyeahyun2 <hgass66@naver.com>
2022-10-25 01:34:08 +0900

    updated_teamProject

--- 실습/2조_서울뮤직페스티벌/script/sec4.js
+++ 실습/2조_서울뮤직페스티벌/script/sec4.js
@@ -4,17 +4,29 @@ const wrapInner = wrap.querySelector('.wrapInner');
 const anchor = wrapInner.querySelectorAll('a');
 
 // wrapInner.prepend(anchor[anchor.length-1])
-const anClone0 = anchor[0].cloneNode(true);
-const anClone1 = anchor[1].cloneNode(true);
+const anClone1 = anchor[0].cloneNode(true);
 const anClone2 = anchor[2].cloneNode(true);
-// console.log(anClone0);
-wrapInner.append(anClone0);
+// console.log(anClone1);
+wrapInner.append(anClone1);
 wrapInner.prepend(anClone2);
-let n = 20;
-// wrapInner.style.transform = `translateX(-20vw)`;
+
+// 변수 선언
+const Newanchor = wrapInner.querySelectorAll('a');
+let transX = 0; // translateX
+let NewA_index = 1; // anchor index number
+// 로딩 직후 값(초기화)
+wrapInner.style.transform = `translateX(${transX}vw)`;
+Newanchor[NewA_index].classList.add("mainSlide");
+
+// 슬라이드 로테이션? 스크립트
 let next = ()=>{
-  let NewAnchor = wrapInner.querySelectorAll('a');
-  // wrapInner.style.transform = `translateX(-${n}vw)`;
+  transX += 20;
+  Newanchor[NewA_index].classList.remove("mainSlide");
+  if(NewA_index >= 3) NewA_index = 0;
+  NewA_index++;
+  console.log(transX);
+  wrapInner.style.transform = `translateX(-${transX}vw)`;
+  Newanchor[NewA_index].classList.add("mainSlide");
+  if(transX >= 40) transX = -20;
 }
-// setInterval(next, 1000);
-// 
+setInterval(next, 2000);
